f74fdb3eeb6734c5361aab34260af31117863e7a
Kaizouku14 <mandaalv72@gmail.com>
2024-11-06 12:27:10 +0800

    feat(server): setup trpc

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { PropsWithChildren } from 'react'
+
+const Layout = (props : PropsWithChildren) => {
+  return (
+    <div>
+        {props.children}
+    </div>
+  )
+}
+
+export default Layout;
---
+++ src/server/trpc.ts
@@ -0,0 +1,31 @@
+import { initTRPC } from "@trpc/server";
+import superjson from "superjson";
+import { ZodError } from "zod";
+
+export const createTRPCContext = async (opts: { headers: Headers }) => {
+  return {
+    ...opts,
+  };
+};
+
+/**
+ * This is where the tRPC API is initialized, connecting the context and transformer. We also parse
+ * ZodErrors so that you get typesafety on the frontend if your procedure fails due to validation
+ * errors on the backend.
+ */
+const t = initTRPC.context<typeof createTRPCContext>().create({
+  transformer: superjson,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
+  },
+});
+
+export const createTRPCRouter = t.router;
+export const publicProcedure = t.procedure;
