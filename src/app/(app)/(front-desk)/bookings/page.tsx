9a8702fb6d9dae9bedcef0e1364f298ed01f7940
Kaizouku14 <mandaalv72@gmail.com>
2024-11-18 18:19:48 +0800

    feat(bookings): add tab component

--- src/app/(app)/(front-desk)/bookings/page.tsx
+++ src/app/(app)/(front-desk)/bookings/page.tsx
@@ -1,21 +1,33 @@
-import { ChevronRight } from 'lucide-react'
-import React from 'react'
-import BookingForm from './_components/forms/booking-form'
+import { ChevronRight } from "lucide-react";
+import React from "react";
+import BookingForm from "./_components/forms/booking-form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Page = () => {
   return (
     <div className="flex flex-col p-1 w-full">
-       <div className='flex items-center gap-x-1'>
-          <span className='font-medium'>InnControl</span>
-          <ChevronRight size={19}/>
-          <span className='font-medium'>Bookings</span>
-       </div>
+      <div className="flex items-center gap-x-1">
+        <span className="font-medium">InnControl</span>
+        <ChevronRight size={19} />
+        <span className="font-medium">Bookings</span>
+      </div>
 
-       <div className='flex-1 py-3 md:mr-8 mr-6 '>
-          <BookingForm/>
-       </div>
+      <div className="flex-1 py-3 md:mr-8 mr-6">
+        <Tabs defaultValue="booking-form" >
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="booking-form">Reservation</TabsTrigger>
+            <TabsTrigger value="transaction-table">transaction</TabsTrigger>
+          </TabsList>
+          <TabsContent value="booking-form">
+              <BookingForm />
+          </TabsContent>
+          <TabsContent value="transaction-table">
+             
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
