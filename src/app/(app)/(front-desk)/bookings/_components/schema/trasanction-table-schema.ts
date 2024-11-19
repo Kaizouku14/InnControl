572c9ea45b121fdf2f032b4e5e07cf345158d5af
Kaizouku14 <mandaalv72@gmail.com>
2024-11-19 12:19:18 +0800

    feat(booking): add transaction table schema

--- src/app/(app)/(front-desk)/bookings/_components/schema/trasanction-table-schema.ts
+++ src/app/(app)/(front-desk)/bookings/_components/schema/trasanction-table-schema.ts
@@ -1,7 +1,20 @@
 import { z } from "zod";
 
 export const transactionSchema = z.object({
-   
+    transaction_id : z.number(),
+    guest_fullname : z.string(),
+    room_no :  z.string(),
+    check_in : z.date(),  
+    check_out : z.date(),
+    no_of_nights :  z.number(),
+    payment_amount : z.number(),
+    payment_date : z.date(),
+    payment_method :  z.enum(["Cash", "Credit-card", "E-Cash"]),
+    booking_type :  z.enum(["Online", "Walk-in"]),
+    additional_service :  z.enum(["Breakfast"]),
+    outstanding_balance :  z.number(),
+    discount :  z.enum(["pwd","senior"]),
+    status :  z.enum(["active", "canceled","processed"]),
 });
 
 export type Transaction = z.infer<typeof transactionSchema>;
--- src/server/api/routers/transaction.ts
+++ src/server/api/routers/transaction.ts
@@ -1,13 +1,20 @@
-import { getTotalRevenue, getVisitorDistribution } from "@/lib/api/app/transaction/query";
+import {
+  getAllTransaction,
+  getTotalRevenue,
+  getVisitorDistribution,
+} from "@/lib/api/app/transaction/query";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const transactionRouter = createTRPCRouter({
-    getVisitorDistribution: publicProcedure.query(async () => {
-        return await getVisitorDistribution();
-    }),
-    getTotalRevenue: publicProcedure.query(async () => {
-        return await getTotalRevenue();
-    }),
+  getAllTransaction: publicProcedure.query(async () => {
+    return await getAllTransaction();
+  }),
+  getVisitorDistribution: publicProcedure.query(async () => {
+    return await getVisitorDistribution();
+  }),
+  getTotalRevenue: publicProcedure.query(async () => {
+    return await getTotalRevenue();
+  }),
 });
 
 export type Transaction = typeof transactionRouter;
