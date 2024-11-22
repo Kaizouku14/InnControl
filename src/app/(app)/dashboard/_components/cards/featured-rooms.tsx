b1228e7e1d1a7aa80ee3d254f3248053b24c71ad
karldevx <karlmpaguio@gmail.com>
2024-11-23 02:48:24 +0800

    feat(dashboard): add featured room skeleton

--- src/app/(app)/dashboard/_components/cards/featured-rooms.tsx
+++ src/app/(app)/dashboard/_components/cards/featured-rooms.tsx
@@ -16,9 +16,10 @@ import ER2 from "../../../../assets/er-2.jpg";
 import SRDeluxe from "../../../../assets/sr-deluxe.jpg";
 import SRPrime from "../../../../assets/sr-prime.jpg";
 import SRPrimier from "../../../../assets/sr-premier.jpg";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const FeaturedRooms = () => {
-  const { data } = api.transaction.getMostFeaturedRooms.useQuery();
+  const { data, isLoading } = api.transaction.getMostFeaturedRooms.useQuery();
 
   const roomImages: Record<string, StaticImageData> = {
     "SR Deluxe": SRDeluxe,
@@ -42,13 +43,19 @@ const FeaturedRooms = () => {
       <CardDescription>
         Top 3 most booked rooms in the last month.
       </CardDescription>
-      <CardContent className="grid md:grid-cols-3 gap-2 py-4">
-        {data &&
-          data.slice(0, 3).map((room, index) => (
+      <CardContent className="grid md:grid-cols-3 gap-2 py-2.5">
+        {isLoading ? (
+          <>
+            <Skeleton className="w-full h-56 rounded-lg" />
+            <Skeleton className="w-full rounded-lg" />
+            <Skeleton className="w-full rounded-lg" />
+          </>
+        ) : (
+          data?.slice(0, 3).map((room, index) => (
             <Badge
               key={index}
               variant="outline"
-              className="rounded-lg flex flex-col items-center gap-2 py-2"
+              className="rounded-lg flex flex-col items-center gap-2 py-1"
             >
               <Image
                 src={roomImages[room?.[0]]}
@@ -59,7 +66,8 @@ const FeaturedRooms = () => {
               />
               <span className="text-base">{roomNames[room?.[0]]}</span>
             </Badge>
-          ))}
+          ))
+        )}
       </CardContent>
     </Card>
   );
