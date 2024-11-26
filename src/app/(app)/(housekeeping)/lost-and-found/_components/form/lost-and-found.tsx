e6f2284e37f55199618da53d32ba043998ad4f9a
Kaizouku14 <mandaalv72@gmail.com>
2024-11-26 12:28:32 +0800

    feat(lost&found): add calendar picker

--- src/app/(app)/(housekeeping)/lost-and-found/_components/form/lost-and-found.tsx
+++ src/app/(app)/(housekeeping)/lost-and-found/_components/form/lost-and-found.tsx
@@ -14,29 +14,54 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { CalendarIcon, Pencil } from "lucide-react";
+import { useState } from "react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
 // import SubmitButton from "@/components/forms/submit-button";
 
 const LostAndFound = () => {
+  const [userAvatar, setUserAvatar] = useState<File>();
+  const [userAvatarUrl, setUserAvatarUrl] = useState<string>(
+    "https://github.com/shadcn.png"
+  );
+
   const form = useForm<z.infer<typeof LostAndFoundSchema>>({
     resolver: zodResolver(LostAndFoundSchema),
     defaultValues: {
       item_lost: "",
       item_color: "",
+      issued_date: new Date(),
     },
   });
 
   function onSubmit(values: z.infer<typeof LostAndFoundSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+    if (!userAvatar) console.log(values);
   }
+
+  const handleChangeProfile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      setUserAvatar(file);
+      setUserAvatarUrl(URL.createObjectURL(file));
+    }
+  };
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mr-8 md:mr-12">
-
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 mr-8 md:mr-12"
+      >
         <div className="grid md:grid-cols-2 items-center gap-4">
           <FormField
             control={form.control}
@@ -67,6 +92,66 @@ const LostAndFound = () => {
                 <FormDescription>
                   This is your public display name.
                 </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="grid md:grid-cols-2 gap-4">
+          <Avatar className="size-56 relative">
+            <AvatarImage src={userAvatarUrl} />
+            <AvatarFallback>CN</AvatarFallback>
+
+            <button
+              className="absolute right-1 bottom-1 bg-slate-300 rounded-full p-2 hover:bg-slate-400 transition-colors duration-150 cursor-pointer"
+              onClick={() => document.getElementById("profile-upload")?.click()}
+            >
+              <Pencil size={15} aria-hidden="true" />
+              <input
+                id="profile-upload"
+                type="file"
+                accept="image/*"
+                className="hidden"
+                onChange={handleChangeProfile}
+              />
+            </button>
+          </Avatar>
+
+          <FormField
+            control={form.control}
+            name="issued_date"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>
+                  <span>Date Found</span>
+                  <span className="text-red-500 ml-1">*</span>
+                </FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button variant={"secondary"} className="shadow">
+                        {field.value ? (
+                          <span>{new Date(field.value).toDateString()}</span>
+                        ) : (
+                          <span>Check In Date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={new Date(field.value)}
+                      onSelect={field.onChange}
+                      disabled={(date: Date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
                 <FormMessage />
               </FormItem>
             )}
